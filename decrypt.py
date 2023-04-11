fd0db4bb30cee7868a18a4c9d63cb14913b76688
TheVirtualPhysicist <64352871+TheVirtualPhysicist@users.noreply.github.com>
2023-04-11 18:01:30 +0200

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,21 @@
+from cryptography.fernet import Fernet
+import os
+
+key = os.environ['FERNET']
+fernet = Fernet(key)
+
+# decrypting
+print("Loading crypted files...")
+
+def decrypt(fileName):
+	with open(fileName,"rb") as file:
+		ctext = file.read()
+		text = fernet.decrypt(ctext)
+	with open(f'{fileName.split(".")[0]}.py'[1:], 'wb') as file:
+		file.write(text)
+
+# files = ["cservice.cpy","cBot.cpy","cTrader.cpy"]
+# for i in files:
+# 	decrypt(i)
+
+print("Files decrypted.")
